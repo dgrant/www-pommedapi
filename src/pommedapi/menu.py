ca2bfaf106870e0b27c95585e9135c2c9ee22435
David Grant <davidgrant@gmail.com>
2013-11-17 21:41:06 -0800

    Removed Login and Register buttons

--- src/pommedapi/menu.py
+++ src/pommedapi/menu.py
@@ -9,15 +9,15 @@ class TestMenu(Menu):
         nodes = []
         if request.user.is_authenticated():
             logout_url = reverse('django.contrib.auth.views.logout')
-            n1 = NavigationNode(_('Logout'), logout_url, 1)
-            nodes.append(n1)
+#            n1 = NavigationNode(_('Logout'), logout_url, 1)
+#            nodes.append(n1)
         else:
             login_url = reverse('django.contrib.auth.views.login')
             registration_url = reverse('registration_register')
-            n1 = NavigationNode(_('Login'), login_url, 1)
-            n2 = NavigationNode(_('Register'), registration_url, 2)
-            nodes.append(n1)
-            nodes.append(n2)
+#            n1 = NavigationNode(_('Login'), login_url, 1)
+#            n2 = NavigationNode(_('Register'), registration_url, 2)
+#            nodes.append(n1)
+#            nodes.append(n2)
         return nodes
 
 menu_pool.register_menu(TestMenu)
